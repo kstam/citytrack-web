13623c87945a88af63c84a05191e268334f48c89
kstamatoukos <kstamatoukos@ebay.com>
2014-12-16 17:37:33 +0100

    Added basic map/areaSelect interaction

--- src/js/client/main.js
+++ src/js/client/main.js
@@ -14,15 +14,18 @@ var areaSection = $('.areaDropdown:first')[0];
 
 var areaWidget = new SearchDropdownWidget(areaSection);
 new SearchDropdownWidget(keywordSection);
-new MapWidget(mainMapSection);
+var mainMapWidget = new MapWidget(mainMapSection);
 
 areaWidget.onChange(function() {
-    console.log(areaWidget.getValue());
+    var area = areaMap[areaWidget.getValue()];
+    mainMapWidget.setView(area);
 });
 
+var areaMap = {};
 areaService.getAreas(function(areas) {
     var selectViewModel = [];
     areas.forEach(function(area) {
+        areaMap[area.name] = area;
         selectViewModel.push({
             label: area.name,
             value: area.name
