1901171b59a2fc679da83971a6576895b3963523
kstamatoukos <kstamatoukos@ebay.com>
2014-12-16 13:19:21 +0100

    Created select dropdown components for the header. Makes use of pre-compiled Handlebars templates for html rendering where possible

---
+++ src/js/client/widgets/SearchDropdownWidget.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var $ = require('jquery');
+var selectize = require('selectize');
+var utils = require('common/utils');
+var domify = require('domify');
+var template = require('widgets/search-dropdown-widget.hbs');
+
+var SearchDropdownWidget = function SearchDropdownWidget(theParent) {
+    var element = domify(template());
+    var parent = utils.getElement(theParent);
+    var selectize;
+
+    var initialize = function() {
+        $(parent).append(element);
+        $(element).selectize({
+            valueField: 'value',
+            labelField: 'label',
+            searchField: 'value'
+        });
+        selectize = element.selectize;
+    };
+
+    var validateOption = function(option) {
+        if (!(option && option.value && option.label)) {
+            throw new Error('Attemped to add invalid option ' + option);
+        }
+    };
+
+    var selectValue = function(value) {
+        selectize.setValue(value);
+    };
+
+    var getValue = function() {
+        var value = selectize.getValue();
+        return (value) ? value : undefined;
+    };
+
+    var getAvailableValues = function() {
+        return Object.keys(selectize.options);
+    };
+
+    var setOptions = function(data) {
+        if (!utils.isArray(data)) {
+            throw new Error('data should be an array');
+        }
+
+        data.forEach(function(entry) {
+            validateOption(entry);
+        });
+
+        var selectedValue = getValue();
+        selectize.clearOptions();
+        selectize.load(function(callback) {
+            callback(data);
+        });
+        selectValue(selectedValue);
+    };
+
+    var onChange = function(listener) {
+        selectize.on('change', listener);
+    };
+
+    var onType = function(listener) {
+        selectize.on('type', listener);
+    };
+
+    initialize();
+
+    return {
+        getAvailableValues: getAvailableValues,
+        setData: setOptions,
+        selectValue: selectValue,
+        getValue: getValue,
+
+        onChange: onChange,
+        onType: onType
+    };
+};
+
+SearchDropdownWidget.SELECTION_CHANGED_EVT = 'SearchDropdown:SelectionChanged';
+
+module.exports = SearchDropdownWidget;
---
+++ test/client/widgets/SearchDropdownWidgetSpec.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var expect = require('../../chaiExpect');
+var $ = require('jquery');
+var SearchDropdownWidget = require('client/widgets/SearchDropdownWidget.js');
+var sinon = require('sinon');
+
+describe('SearchDropdownWidget', function() {
+
+    var CONTAINER_ID = 'selectContainerId';
+    var searchWidget;
+    var $selectInput;
+
+    beforeEach(function() {
+        var container = $('<div id="' + CONTAINER_ID + '"></div>');
+        $(document.body).prepend(container);
+        searchWidget = new SearchDropdownWidget(container[0]);
+        $selectInput = $(container).find('input:first');
+    });
+
+    afterEach(function() {
+        $('#' + CONTAINER_ID).remove();
+    });
+
+    describe('initialize data', function() {
+
+        it('should have no data on initialization', function() {
+            expect(searchWidget.getAvailableValues()).to.deep.equal([]);
+        });
+
+        it('should allow adding an array of rows', function() {
+            var data = [
+                {label: 'Kostas', value: 'k'},
+                {label: 'Giorgos', value: 'g'}
+            ];
+            searchWidget.setData(data);
+            expect(searchWidget.getAvailableValues()).to.deep.equal(['k', 'g']);
+        });
+
+        it('should keep the value selected if it is still in the new data', function() {
+            searchWidget.setData([{label: 'Kostas', value: 'k'},
+                {label: 'Giorgos', value: 'g'}]);
+            searchWidget.selectValue('g');
+            searchWidget.setData([{label: 'Nikos', value: 'n'},
+                {label: 'Giorgos', value: 'g'}]);
+            expect(searchWidget.getValue()).to.equal('g');
+        });
+
+        it('should reset the value selected if it is not in the new data', function() {
+            searchWidget.setData([{label: 'Kostas', value: 'k'},
+                {label: 'Giorgos', value: 'g'}]);
+            searchWidget.selectValue('k');
+            searchWidget.setData([{label: 'Nikos', value: 'n'},
+                {label: 'Giorgos', value: 'g'}]);
+            expect(searchWidget.getValue()).to.be.undefined();
+        });
+    });
+
+    describe('select values', function() {
+
+        beforeEach(function() {
+            searchWidget.setData([
+                {label: 'Kostas', value: 'k'},
+                {label: 'Giorgos', value: 'g'}
+            ]);
+        });
+
+        it('should have no selected value at initialization', function() {
+            expect(searchWidget.getValue()).to.be.undefined();
+        });
+
+        it('should allow selecting a value', function() {
+            searchWidget.selectValue('k');
+            expect(searchWidget.getValue()).to.equal('k');
+        });
+
+        it('should throw an error if trying to select a value that does not exist', function() {
+            searchWidget.selectValue('k');
+            searchWidget.selectValue('wrong');
+            expect(searchWidget.getValue()).to.be.undefined();
+        })
+    });
+
+    describe('events', function() {
+
+        beforeEach(function() {
+            searchWidget.setData([
+                {label: 'Kostas', value: 'k'},
+                {label: 'Giorgos', value: 'g'}
+            ]);
+        });
+
+        it('should allow registering an on change listener', function() {
+            var callback = sinon.spy();
+            searchWidget.onChange(callback);
+
+            searchWidget.selectValue('k');
+            expect(callback).to.have.been.calledWith('k');
+        });
+
+        it('should allow registering an onType listener', function() {
+            var callback = sinon.spy();
+            searchWidget.onType(callback);
+
+            $selectInput.attr('value', 'hello');
+            $selectInput.trigger('keyup');
+
+            expect(callback).to.have.been.calledWith('hello');
+        });
+    });
+});
