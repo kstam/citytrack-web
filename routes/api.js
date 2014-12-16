8b47fd80dae011769da9c4f366c7f35ab385c2a4
kstamatoukos <kstamatoukos@ebay.com>
2014-12-16 17:33:25 +0100

    Fixed the area long/lat for testing it in the browser

--- routes/api.js
+++ routes/api.js
@@ -5,24 +5,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 
 var areas = [
-    new Area('Athens',
-        {lat: 20, lng:50},
-        [{lat: 10, lng: 30}, {lat: 30, lng: 60}]),
-    new Area('Zurich',
-        {lat: 20, lng:50},
-        [{lat: 10, lng: 30}, {lat: 30, lng: 60}]),
-    new Area('London',
-        {lat: 20, lng:50},
-        [{lat: 10, lng: 30}, {lat: 30, lng: 60}])
+    new Area("London", {lat: 51.507222, lng: -0.1275}, [{lat: 51.27, lng: -0.49},
+        {lat: 51.7, lng: 0.24}]),
+    new Area("Athens", {lat: 37.966667, lng: 23.716667}, [{lat: 37.8, lng: 23.5},
+        {lat: 38.2, lng: 24}]),
+    new Area("Vienna", {lat: 48.2, lng: 16.366667}, [{lat:  48.11, lng: 16.18},
+        {lat: 48.33, lng: 16.58}])
 ];
 
 router.get('/areas', function(req, res) {
-   res.send(areas);
+    res.send(areas);
 });
 
 module.exports = router;
