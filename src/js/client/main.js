674dfd4902e435b1621837396064891a75f44891
kstamatoukos <kstamatoukos@ebay.com>
2014-12-16 13:24:54 +0100

    Make use of the created widgets in the page

--- src/js/client/main.js
+++ src/js/client/main.js
@@ -1,9 +1,32 @@
 'use strict';
 
+require('es5-shim');
 var $ = require('jquery');
 var MapWidget = require('client/widgets/MapWidget');
+var SearchDropdownWidget = require('client/widgets/SearchDropdownWidget');
+
+var areaService = require('client/services/areaService');
 
 // elements
-var mapContainer = $('.mainMapContainer:first')[0];
+var mainMapSection = $('.mainMapSection:first')[0];
+var keywordSection = $('.keywordBox:first')[0];
+var areaSection = $('.areaDropdown:first')[0];
 
-MapWidget(mapContainer);
+var areaWidget = new SearchDropdownWidget(areaSection);
+new SearchDropdownWidget(keywordSection);
+new MapWidget(mainMapSection);
+
+areaWidget.onChange(function() {
+    console.log(areaWidget.getValue());
+});
+
+areaService.getAreas(function(areas) {
+    var selectViewModel = [];
+    areas.forEach(function(area) {
+        selectViewModel.push({
+            label: area.name,
+            value: area.name
+        });
+    });
+    areaWidget.setData(selectViewModel);
+});
