b180cd156055ccfaa324f35f5175079a5cb28650
kstamatoukos <kstamatoukos@ebay.com>
2014-12-17 01:23:58 +0100

    Changed the main entry point to use the HeaderWidget that was just implemented

--- src/js/client/main.js
+++ src/js/client/main.js
@@ -3,33 +3,18 @@
 require('es5-shim');
 var $ = require('jquery');
 var MapWidget = require('client/widgets/MapWidget');
-var SearchDropdownWidget = require('client/widgets/SearchDropdownWidget');
-
-var areaService = require('client/services/areaService');
+var HeaderWidget = require('client/widgets/HeaderWidget');
+var eventBus = require('client/eventBus');
+var appState = require('client/appState');
 
 // elements
+var headerSection = $('.headerSection:first')[0];
 var mainMapSection = $('.mainMapSection:first')[0];
-var keywordSection = $('.keywordBox:first')[0];
-var areaSection = $('.areaDropdown:first')[0];
 
-var areaWidget = new SearchDropdownWidget(areaSection);
-new SearchDropdownWidget(keywordSection);
+// widgets
+var headerWidget = new HeaderWidget(headerSection);
 var mainMapWidget = new MapWidget(mainMapSection);
 
-areaWidget.onChange(function() {
-    var area = areaMap[areaWidget.getValue()];
-    mainMapWidget.setView(area);
-});
-
-var areaMap = {};
-areaService.getAreas(function(areas) {
-    var selectViewModel = [];
-    areas.forEach(function(area) {
-        areaMap[area.name] = area;
-        selectViewModel.push({
-            label: area.name,
-            value: area.name
-        });
-    });
-    areaWidget.setData(selectViewModel);
+eventBus.on(appState.AREA_CHANGED_EVT, function(newArea) {
+    mainMapWidget.setView(newArea);
 });
