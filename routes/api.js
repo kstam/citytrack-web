08ef66c4c79c7cbf624d192e21fe16b662d5117d
kstamatoukos <kstamatoukos@ebay.com>
2015-02-16 11:24:01 +0100

    Pointing to the deployed services from the api in the frontend app

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,11 @@
 var express = require('express');
 var Area = require('../src/js/model/Area');
+var http = require('http');
+var config = require('./config');
+var proxy = require('express-http-proxy');
 
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res) {
-    res.send('respond with a resource');
+module.exports = proxy(config.SERVICES_HOST, {
+    forwardPath: function(req, res) {
+        return config.SERVICES_ENDPOINT + require('url').parse(req.url).path;
+    }
 });
-
-
-var areas = [
-    new Area("London", {lat: 51.507222, lng: -0.1275}, [{lat: 51.27, lng: -0.49},
-        {lat: 51.7, lng: 0.24}]),
-    new Area("Athens", {lat: 37.966667, lng: 23.716667}, [{lat: 37.8, lng: 23.5},
-        {lat: 38.2, lng: 24}]),
-    new Area("Vienna", {lat: 48.2, lng: 16.366667}, [{lat:  48.11, lng: 16.18},
-        {lat: 48.33, lng: 16.58}])
-];
-
-router.get('/areas', function(req, res) {
-    res.send(areas);
-});
-
-module.exports = router;
---
+++ routes/config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var urlConfig = {
+    SERVICES_HOST: 'citytrack.magellan.imis.athena-innovation.gr',
+    SERVICES_ENDPOINT: '/citytrack-api'
+};
+
+module.exports = urlConfig;
