c021153dedf08b6341c597ea608c29765c868801
Konstantinos Stamatoukos <kstamatoukos@ks-mac.local>
2015-02-25 16:33:25 +0100

    Implemented area handling in the controller

--- src/js/client/controllers/AreaSelectController.js
+++ src/js/client/controllers/AreaSelectController.js
@@ -1,16 +1,63 @@
 'use strict';
 
-module.exports = function($scope) {
+var angular = require('../shims/angular');
+
+module.exports = function($scope, areaService) {
 
     var initConfig = function() {
         $scope.config = {
+            create: false,
+            maxItems: 1,
+            valueField: 'name',
+            labelField: 'name',
+            openOnFocus: true,
             placeholder: 'Where...'
         };
     };
 
-    var initialize = function() {
+    var initAreas = function() {
+        $scope.areaMap = {};
         $scope.areas = [];
+        areaService.getAreas(function(err, areas) {
+            if (err instanceof Error) {
+                //TODO
+                return;
+            }
+            $scope.$apply(function() {
+                angular.forEach(areas, function(value) {
+                    $scope.areaMap[value.getName()] = value;
+                });
+            });
+        });
+    };
+
+    var initCurrentArea = function() {
+        areaService.getCurrentArea(function(err, currentArea) {
+            if (err instanceof Error) {
+                //TODO;
+                return;
+            }
+            $scope.$apply(function() {
+                $scope.areaMap[currentArea.getName()] = currentArea;
+                $scope.currentArea = currentArea;
+            });
+        });
+    };
+
+    $scope.$watch('areaMap', function(current, old) {
+        if (angular.equals(current, old)) {
+            return;
+        }
+        $scope.areas = [];
+        angular.forEach(current, function(value) {
+            $scope.areas.push(value);
+        });
+    }, true);
+
+    var initialize = function() {
         initConfig();
+        initAreas();
+        initCurrentArea();
     };
 
     initialize();
--- test/client/controllers/AreaSelectControllerSpec.js
+++ test/client/controllers/AreaSelectControllerSpec.js
@@ -4,30 +4,80 @@ require('angular');
 require('angular-mocks');
 var controller = require('client/controllers/AreaSelectController');
 var constants = require('client/config/constants');
+var testUtils = require('../../testCommons/testUtils');
 
 describe('AreaSelectController', function() {
-    var scope;
+    var scope, mockedAreaService;
 
-    beforeEach(function() {
-        scope = {};
-        controller(scope);
-    });
+    beforeEach(inject(function($rootScope) {
+        scope = $rootScope.$new();
+        createMockedAreaService();
+        controller(scope, mockedAreaService);
+    }));
 
     it('should setup the config of the selectize component', function() {
         expect(scope.config).not.to.be.undefined;
     });
 
     it('should initialize the areas to an empty array', function() {
-        expect(scope.areas).to.deep.equal([]);
+        expect(scope.areaMap).to.deep.equal({});
+    });
+
+    it('should start with an undefined currentArea', function() {
+        expect(scope.currentArea).to.equal(undefined);
     });
 
     describe('initializes areas by querying the area service and', function() {
+        it('should load the areas from the server and create a map for them', function() {
+            mockServiceForSuccess();
+            controller(scope, mockedAreaService);
+            expect(scope.areaMap['Athens']).not.to.equal(undefined);
+            expect(scope.areaMap['London']).not.to.equal(undefined);
+        });
+
         it('should configure the current user address', function() {
-
+            mockServiceForSuccess();
+            controller(scope, mockedAreaService);
+            expect(scope.currentArea).not.to.equal(undefined);
+            expect(scope.areaMap[constants.CURRENT_AREA_ID]).not.to.equal(undefined);
         });
+    });
 
-        it('should configure the available areas from the server', function() {
+    it('should update the options array when the areaMap changes', function() {
+        scope.$digest();
+        scope.areaMap.this = testUtils.createRandomArea('Berlin');
+        scope.$digest();
+        expect(scope.areas.length).to.equal(1);
+        expect(scope.areas[0].getName()).to.equal('Berlin');
+    });
 
-        });
-    })
+    //Test helpers
+
+    function createMockedAreaService() {
+        mockedAreaService = {
+            getAreas: function() {
+            },
+            getCurrentArea: function() {
+            }
+        };
+    }
+
+    function mockServiceForSuccess() {
+        var mockedData = [testUtils.createRandomArea('Athens'), testUtils.createRandomArea('London')];
+        mockedAreaService.getAreas = function(callback) {
+            callback(undefined, mockedData);
+        };
+        mockedAreaService.getCurrentArea = function(callback) {
+            callback(undefined, testUtils.createRandomArea(constants.CURRENT_AREA_ID));
+        };
+    }
+
+    function mockServiceForError() {
+        mockedAreaService.getAreas = function(callback) {
+            callback(new Error('message'));
+        };
+        mockedAreaService.getCurrentArea = function(callback) {
+            callback(new Error('message'));
+        };
+    }
 });
