0a6d3bc18f7f912ec6339d52e961810d71fe9f6e
Konstantinos Stamatoukos <kstamatoukos@ks-mac.local>
2015-02-25 16:32:37 +0100

    Added services to the controller dependencies and wired proper scopes

--- src/js/client/controllers/controllers.js
+++ src/js/client/controllers/controllers.js
@@ -1,15 +1,16 @@
 'use strict';
 
 var angular = require('../shims/angular');
+var services = require('../services/services');
 
-var citytrackControllers = angular.module('citytrack.controllers', []);
+var citytrackControllers = angular.module('citytrack.controllers', [services.name]);
 
 citytrackControllers.controller('CitytrackMainController', ['$scope', function($scope) {
     require('./CitytrackMainController')($scope);
 }]);
 
-citytrackControllers.controller('AreaSelectController', ['$scope', function($scope) {
-    require('./AreaSelectController')($scope);
+citytrackControllers.controller('AreaSelectController', ['$scope', 'AreaService', function($scope, areaService) {
+    require('./AreaSelectController')($scope, areaService);
 }]);
 
 module.exports = citytrackControllers;
--- test/client/controllers/controllersSpec.js
+++ test/client/controllers/controllersSpec.js
@@ -8,15 +8,17 @@ describe('controllers', function() {
 
     beforeEach(angular.mock.module(controllers.name));
 
-    it('should define a CitytrackMainController', inject(function($controller) {
-        var scope = {};
-        var controller = $controller('CitytrackMainController', {$scope:scope});
+    it('should define a CitytrackMainController', inject(function($rootScope, $controller) {
+        var controller = $controller('CitytrackMainController', {$scope:$rootScope});
         expect(controller).to.not.be.null();
     }));
 
-    it('should define an AreaSelectController', inject(function($controller) {
-        var scope = {};
-        var controller = $controller('AreaSelectController', {$scope:scope});
+    it('should define an AreaSelectController', inject(function($rootScope, $controller) {
+        var areaService = {
+            getAreas: function() {},
+            getCurrentArea: function() {}
+        };
+        var controller = $controller('AreaSelectController', {$scope:$rootScope, AreaService: areaService});
         expect(controller).to.not.be.null();
     }));
 });
