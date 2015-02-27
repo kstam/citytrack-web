6b5d377e18f50c1ed25aef78dd54f98b9e0b2a3c
Konstantinos Stamatoukos <kstamatoukos@ks-mac.local>
2015-02-27 21:51:58 +0100

    Added forever ignore and server file for launching the app with forever

---
+++ server.js
@@ -0,0 +1,20 @@
+/*
+    This script is used in order to deploy the app using forever
+ */
+'use strict';
+
+var forever = require('forever'),
+    child = new(forever.Monitor)('app.js', {
+        'silent': false,
+        'pidFile': 'pids/app.pid',
+        'watch': true,
+        'watchDirectory': '.',      // Top-level directory to watch from.
+        'watchIgnoreDotFiles': true, // whether to ignore dot files
+        'watchIgnorePatterns': [], // array of glob patterns to ignore, merged with contents of watchDirectory + '/.foreverignore' file
+        'logFile': 'logs/forever.log', // Path to log output from forever process (when daemonized)
+        'outFile': 'logs/forever.out', // Path to log output from child stdout
+        'errFile': 'logs/forever.err'
+    });
+
+child.start();
+forever.startServer(child);
