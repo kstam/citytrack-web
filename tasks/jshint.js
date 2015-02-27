fe29e7ec4c95563b5b25a8f89b31e71ffa19b4fc
Konstantinos Stamatoukos <kstamatoukos@ks-mac.local>
2015-02-27 13:15:51 +0100

    Added jshint jenkins reporter

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -1,8 +1,11 @@
 var jshint = require('gulp-jshint');
 var gulp = require('gulp');
 var stylish = require('jshint-stylish');
+var checkstyleFileReporter = require('jshint-checkstyle-file-reporter');
 
-gulp.task('jshint', function() {
+process.env.JSHINT_CHECKSTYLE_FILE = 'build/jshint.xml';
+
+gulp.task('jshint:dev', function() {
     return gulp.src([
         './src/js/**/*.js',
         './routes/*.js',
@@ -11,3 +14,15 @@ gulp.task('jshint', function() {
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
 });
+
+gulp.task('jshint:jenkins', function() {
+    return gulp.src([
+        './src/js/**/*.js',
+        './routes/*.js',
+        'app.js'
+    ])
+        .pipe(jshint())
+        .pipe(jshint.reporter(checkstyleFileReporter));
+});
+
+gulp.task('jshint', ['jshint:jenkins', 'jshint:dev']);
