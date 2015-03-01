9bd52c4ad4fef2e20a6e0b3539cfd80c217aff2d
Konstantinos Stamatoukos <kstamatoukos@ks-mac.local>
2015-03-01 18:44:00 +0100

    Fixed paths in prepare deploy

--- bin/deploy/prepare-deploy.sh
+++ bin/deploy/prepare-deploy.sh
@@ -2,8 +2,8 @@
 
 # Create directories and permissions
 sudo mkdir -p /var/www/citytrack-web
-sudo chown -R kstam citytrack-web
-sudo chown -R kstam:kstam citytrack-web
+sudo chown -R kstam /var/www/citytrack-web
+sudo chown -R kstam:kstam /var/www/citytrack-web
 
 # Copy citytrack-web.conf to services
 sudo cp citytrack-web.conf /etc/init/
