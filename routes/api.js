beb8e4bea8259f0268a6fd023b27531acee04d77
Kostas Stamatoukos <kstamatoukos@hotmail.com>
2015-04-27 14:34:32 +0200

    Fixing server shutdown in case of API not-reachable

--- routes/api.js
+++ routes/api.js
@@ -8,8 +8,13 @@ var TIMEOUT = 10 * 60 * 1000; //10min
 
 module.exports = function(req, res) {
     var url = config.SERVICES_HOST + config.SERVICES_ENDPOINT + require('url').parse(req.url).path;
+
     req.pipe(request({
         url: url,
         timeout: TIMEOUT
+    }, function(error){
+        if (error && error.code === 'ECONNREFUSED'){
+            res.status(500).send('Could\'t reach remote server. Proxy failed');
+        }
     })).pipe(res);
 };
