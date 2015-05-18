4bc422eb8ac61bffdbebeaf4bcca0a037e649a77
Kostas Stamatoukos <kstamatoukos@hotmail.com>
2015-05-18 22:26:29 +0200

    Cleanup needless requires

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,7 @@
-var express = require('express');
-var Area = require('../src/js/model/Area');
-var http = require('http');
 var config = require('./config');
 var request = require('request');
 
 var TIMEOUT = 10 * 60 * 1000; //10min
-
 module.exports = function(req, res) {
     var url = config.SERVICES_HOST + config.SERVICES_ENDPOINT + require('url').parse(req.url).path;
 
